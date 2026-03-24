a9f5c7b1416bcd2377e8fcd79dcadd949eec6928
holden.anderson <holden.anderson@students.snow.edu>
2026-03-24 16:28:51 -0600

    Linq notes

--- ConsoleApp12/Program.cs
+++ ConsoleApp12/Program.cs
@@ -1,36 +1,69 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Globalization;
+using System.Linq;
 
 public class Program
 { //probably incomplete notes about LINQ 
         //how not to do it, how to do without it
+        
 	static void Main()
-	{
-		List <int> list = new List<int>() {4,5,6,7,8, 2,5, 1,13, 68, 1,3};
-        foreach ( int number in list)
-        {
-            if (number >4)
-            {
-                filterList.Add(number);
-            }
+    {
+        List<Person> list = BabyMaker();
+        //p equals the things in our person list
+        List<Person> peopleNamedBob = list.Where(p => p.Name == "Bob" && p.Age <10 ).ToList();
+
+        //example                     //woah this one has where and age, filter by name and then transform into the shape of only ages
+        var result = list.Where(p => p.Age > 9).Select(p=>p.Name); //you cant add order by name before select, because then 
+                                                                // then there isnt names anymore, the list is transformed 
+        var result2 = list.Where(p => p.Age > 9)
+                          .OrderBy(p=>p.Name)
+                          .Select(p=>p.Name);
+//using groupby is like going a whole nother level above orderby, you like group a list of lists together. crazy and complex
+
+
+        //any returns a bool asking if the specified thing is in the list
+        //any exist in it? true/false
+        var testing = list.Any(p => p.Name == "Jeff"); //must be exact, using less search letter like "Jef" returns false
+        
+        var testing2 = list.Where(p => p.Name == "Jeff"); //must be exact, using less search letter like "Jef" returns false
+        var firstGuy = testing2.FirstOrDefault();
+        
+        var testing3 = list.OrderBy(p => p.Name == "Jeff"); //This isn't actually link but it will print the list in a Re-ordered way
+
+        // select - transforming
+        // we dont care about the people anymore, only a list of their ages
+        List<int> transformed = list.Select(p => p.Age).ToList();
+
+        //var discounted = order.Select(o +> o.Price * .9).ToList; makes a list of discounted prices, same quantity, different shape
+
+        
+
+        // using .distinct shows you one specific result if Bob is in there multiple times, probably the []first result of Bob
     }
+    //LINQ is a great tool to give us a subset of a List
 
-        //if the number is greater than 4
-    
-    
-
-
-    //with LINQ    you will probably use this because your coworkers are lazy, you will learn it and then you will be faster and lazy too
-    // Linq is built on the backs of generics
-    List<int> result = list.Where(number => number > 4).ToList(); // if ever you run into an IEnumerable, you just make it a List and then you don't have to deal with it
-    
-    //this arrow symbol is a      lambda =>  ,  don't get it confused with the greater or equal to is written the opposite way like this >=
-    //                            number  => this is what each thing in your list is going to be called 
-    //                                                 number > 4 what we actually compare 
-    //                            given number, return number > 4 (nothing smaller than 4 will be returned in this instance)
-    
-    // IEnumerable<T> a sequence of items                  
-
-
+    public static List<Person> BabyMaker()
+    {
+        List<Person> list = new List<Person>()
+        {
+            new Person("Bob", 4),
+            new Person("Bob", 4),
+            new Person("Bob", 4),
+            new Person("Bob", 4),
+            new Person("Bob", 4),
+            new Person("Bob", 4),
+        };
+        return list;
+    }    
+}
+public class Person
+{
+    public string Name {get; set;}
+    public int Age {get; set;}
+    public Person (string name, int age)
+    {
+        Name = name;
+        Age = age;
     }
 }
